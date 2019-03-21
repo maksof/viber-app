7c5a33a0489ba8fe63af0309e3e0169fb89e1538
maksof-Rehan <31398564+maksof-Rehan@users.noreply.github.com>
2019-03-21 16:43:35 +0500

    delete work

--- server/delete.php
+++ server/delete.php
@@ -5,7 +5,22 @@
 		if($fileType[0] == 'NETWORKING') $fileUrl = "tbl_networking.txt";
 		else if($fileType[0] == 'MAPPING') $fileUrl = "tbl_mapping.txt";
 		else if($fileType[0] == 'ROUTING') $fileUrl = "tbl_routing.txt";
-		echo $ipAddress = $fileType[1];
+		$ipAddress = $fileType[1];
+        
+        $myFile = getcwd().DIRECTORY_SEPARATOR."db".DIRECTORY_SEPARATOR.$fileUrl;  
+        $lineBreak = "\n";
+        $file = explode($lineBreak, file_get_contents($myFile));
+        for($i=0; $i < count($file); $i++){
+            $data = explode("#",$file[$i]);
+            echo $data[0] ,$ipAddress;
+            if($data[0] == $ipAddress){
+                unset($file[$i]);
+            }
+        }
+        $file = implode("\n",$file);
+        file_put_contents($myFile, $file);
+        header("Location: ../index.php");
+		die();
 	} else {
 		header("Location: ../index.php");
 		die();
